7c370dfa3205ea0f339e901e85f6471cbe6c74ea
Edmond Leung <edmond@inchoatae.com>
2022-06-06 03:07:21 +0800

    App Bar
    Position: fixed

--- resources/js/Layouts/MailAppLayout.vue
+++ resources/js/Layouts/MailAppLayout.vue
@@ -54,7 +54,7 @@ const logout = () => {
     background-color: rgb(2, 105, 164);
     width: 100%;
     height: 40px;
-    /*position: fixed;*/
+    position: fixed;
   }
   .app-bar-left{
     float:left;
