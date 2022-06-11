e567af1f1bd62df7d074bcb616e06e313a3c7d32
Edmond Leung <edmond@inchoatae.com>
2022-06-11 16:04:41 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\App;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        echo config('app.env');
+        /*if (App::environment('remote')) {
+            echo "ddd";
+            URL::forceScheme('https');
+        }*/
     }
 }
